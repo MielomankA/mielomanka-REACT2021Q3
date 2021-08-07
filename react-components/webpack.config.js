cd1386c4d96c3c55db02bcbce4145ba483d36d27
MielomankA <mielomanka@mail.ru>
2021-08-07 04:32:52 +0300

    feat: add eslint and prettier

--- react-components/webpack.config.js
+++ react-components/webpack.config.js
@@ -1,8 +1,7 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == 'production';
 
@@ -24,7 +23,7 @@ const config = {
         new HtmlWebpackPlugin({
             template: 'index.html',
         }),
-
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
         // Add your plugins here
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],

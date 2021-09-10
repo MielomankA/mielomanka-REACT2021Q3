b9b1bdff835b86c73e6f270af72694151f03d3ad
MielomankA <mielomanka@mail.ru>
2021-09-10 03:52:04 +0300

    feat: add animation

--- react-api/src/App.tsx
+++ react-api/src/App.tsx
@@ -1,30 +1,43 @@
 import React from 'react';
 import './style.css';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { MainPage } from './pages/MainPage';
 import { Header } from '../components/Header';
 import { ErrorPage } from './pages/ErrorPage';
 import { AboutPage } from './pages/AboutPage';
 import { Details } from './pages/DetailsPage';
 
+function Content(): JSX.Element {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition timeout={500} classNames="fade" key={location.key}>
+        <Switch location={location}>
+          <Route exact path="/">
+            <MainPage />
+          </Route>
+          <Route exact path="/about">
+            <AboutPage />
+          </Route>
+          <Route exact path="/details/*">
+            <Details />
+          </Route>
+          <Route exact path="*">
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+}
+
 export const App: React.FC = () => {
   return (
     <Router>
       <Header />
-      <Switch>
-        <Route exact path="/">
-          <MainPage />
-        </Route>
-        <Route exact path="/about">
-          <AboutPage />
-        </Route>
-        <Route exact path="/details/*">
-          <Details />
-        </Route>
-        <Route exact path="*">
-          <ErrorPage />
-        </Route>
-      </Switch>
+      <Content />
     </Router>
   );
 };
